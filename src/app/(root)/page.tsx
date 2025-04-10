ca32f9b546b535cd9f58bec8701e11226af6d1ab
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-04-10 16:03:02 -0400

    small

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-2xl font-semibold flex justify-center items-center h-dvh">
+    <div className="text-4xl font-semibold flex justify-center items-center h-dvh">
       Click <Link href="/documents/123" className="text-blue-500 underline px-1">here</Link> to go to document id
     </div>
   );

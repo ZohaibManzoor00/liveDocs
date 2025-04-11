af84a5f30531f0dacfc17b62e978b173b8475267
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-04-11 13:03:00 -0400

    highlight and text color

--- src/app/documents/[documentId]/_components/toolbar.tsx
+++ src/app/documents/[documentId]/_components/toolbar.tsx
@@ -23,6 +23,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { type Level } from "@tiptap/extension-heading"
 
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -41,6 +42,60 @@ function ToolbarButton({ onClick, isActive, icon: Icon }: ToolbarButtonProps) {
     >
       <Icon className="size-4" />
     </button>
+  );
+}
+
+function HeadingLevelButton() {
+  const { editor } = useEditorStore();
+
+  const headings = [
+    { label: "Normal text", value: 0, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "24px" },
+    { label: "Heading 3", value: 3, fontSize: "20px" },
+    { label: "Heading 4", value: 4, fontSize: "18px" },
+    { label: "Heading 5", value: 5, fontSize: "16px" },
+  ];
+
+  const getCurHeading = () => {
+    for (let level = 1; level <= 5; level++) {
+      if (editor?.isActive("heading", { level })) return `Heading ${level}`;
+    }
+    return "Normal Text";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <span className="truncate">{getCurHeading()}</span>
+          <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+        {headings.map(({ label, value, fontSize }) => (
+          <button
+            key={value}
+            style={{ fontSize }}
+            onClick={() => {
+                if (value === 0) {
+                    editor?.chain().focus().setParagraph().run()
+                } else {
+                    editor?.chain().focus().toggleHeading({ level: value as Level }).run()
+                }
+            }}
+            className={cn(
+              "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+              ((value === 0 && !editor?.isActive("heading")) ||
+                editor?.isActive("heading", { level: value })) &&
+                "bg-neutral-200/80"
+            )}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
 
@@ -164,6 +219,7 @@ export default function Toolbar() {
       },
     ],
   ];
+  
   return (
     <div className="bg-[#F1F4F9] px-2.5 py-.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
       {sections[0].map((item) => (
@@ -173,7 +229,7 @@ export default function Toolbar() {
       <FontFamilyButton />
 
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-      {/* TODO: Heading */}
+      <HeadingLevelButton />
 
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* TODO: Font Size */}

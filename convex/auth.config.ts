2182dc6cf84c262a12bddd7374c52964ea7923ef
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-04-27 18:22:34 -0400

    auth setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://many-corgi-53.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- src/app/(root)/_components/navbar.tsx
+++ src/app/(root)/_components/navbar.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/components/shared/logo";
 import SearchInput from "./search-input";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Navbar() {
   return (
@@ -9,7 +10,7 @@ export default function Navbar() {
         <h3 className="text-xl">Live Docs</h3>
       </div>
       <SearchInput />
-      <div />
+      <UserButton />
     </nav>
   );
 }

19735f779006ad17521cbe57ea290b2047746de0
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-04-28 19:32:17 -0400

    loading and pagination and searching completed

--- src/app/(root)/_components/user-gallery.tsx
+++ src/app/(root)/_components/user-gallery.tsx
@@ -1,20 +1,9 @@
-"use client";
+'use client'
 
-import { PaginationStatus, usePaginatedQuery } from "convex/react";
+import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
-import { Doc } from "../../../../convex/_generated/dataModel";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { LoaderIcon } from "lucide-react";
-import DocumentRow from "./document-row";
-import { Button } from "@/components/ui/button";
 import { paginationAmount } from "@/constants/templates";
+import DocumentsTable from "./documents-table";
 
 export default function UserGallery() {
   const { results, status, loadMore } = usePaginatedQuery(
@@ -22,69 +11,8 @@ export default function UserGallery() {
     {},
     { initialNumItems: paginationAmount },
   );
-  return (
-    <>
-      <DocumentsTable documents={results} loadMore={loadMore} status={status} />
-    </>
-  );
-}
 
-interface DocumentsTableProps {
-  documents: Doc<"documents">[] | undefined;
-  loadMore: (numItems: number) => void;
-  status: PaginationStatus;
-}
-const DocumentsTable = ({
-  documents,
-  loadMore,
-  status,
-}: DocumentsTableProps) => {
   return (
-    <div className="max-w-screen-xl mx-auto px-16 py-6 flex flex-col gap-5">
-      {documents === undefined ? (
-        <div className="flex justify-center items-center h-24">
-          <LoaderIcon className="animate-spin text-muted-foreground size-5" />
-        </div>
-      ) : (
-        <Table>
-          <TableHeader>
-            <TableRow className="hover:bg-transparent border-none">
-              <TableHead>Name</TableHead>
-              <TableHead>&nbsp;</TableHead>
-              <TableHead className="hidden md:table-cell">Shared</TableHead>
-              <TableHead className="hidden md:table-cell">Created at</TableHead>
-            </TableRow>
-          </TableHeader>
-          {documents.length === 0 ? (
-            <TableBody>
-              <TableRow className="hover:bg-transparent">
-                <TableCell
-                  colSpan={4}
-                  className="h-24 text-center text-muted-foreground"
-                >
-                  No documents found
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          ) : (
-            <TableBody>
-              {documents.map((doc) => (
-                <DocumentRow key={doc._id} document={doc} />
-              ))}
-            </TableBody>
-          )}
-        </Table>
-      )}
-      <div className="flex items-center justify-center">
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={() => loadMore(paginationAmount)}
-          disabled={status !== "CanLoadMore"}
-        >
-          {status === "CanLoadMore" ? "Load more" : "End"}
-        </Button>
-      </div>
-    </div>
+    <DocumentsTable documents={results} loadMore={loadMore} status={status} />
   );
-};
+}

33c12c48ba1403edbfa901d8ccb8ff0d5b1ca6fc
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2025-04-28 20:37:47 -0400

    search on homepage

--- src/app/(root)/_components/user-gallery.tsx
+++ src/app/(root)/_components/user-gallery.tsx
@@ -4,11 +4,13 @@ import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
 import { paginationAmount } from "@/constants/templates";
 import DocumentsTable from "./documents-table";
+import { useSearchParam } from "@/hooks/use-search-param";
 
 export default function UserGallery() {
+  const [search] = useSearchParam('search')
   const { results, status, loadMore } = usePaginatedQuery(
     api.documents.get,
-    {},
+    { search },
     { initialNumItems: paginationAmount },
   );
 
